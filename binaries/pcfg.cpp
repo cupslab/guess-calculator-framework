dcc20c2201ee56c9e6938c1e0600279c3c583428
William Melicher <billy@cmu.edu>
2016-01-08 11:30:24 -0500

    Adding more information in the "too many open files" error message.

--- binaries/pcfg.cpp
+++ binaries/pcfg.cpp
@@ -8,14 +8,15 @@
 // Author: Saranga Komanduri
 //   Based on code originally written and published by Matt Weir under the
 //   GPLv2 license.
-// 
+//
 // Modified: Wed May 28 17:07:11 2014
-// 
+//
 // See header file for additional information
 
 // Includes not covered in header file
 #include <cstdio>
 #include <cstdlib>
+#include <errno.h>
 #include "grammar_tools.h"
 
 #include "pcfg.h"
@@ -43,9 +44,17 @@ bool PCFG::loadGrammar(
     const std::string& terminals_folder) {
   FILE *structurefile = fopen(structuresfilename.c_str(), "r");
   if (structurefile == NULL) {
+    int saved_errno = errno;
     perror("Error opening structure file: ");
     fprintf(stderr,
       "Structures filename: %s\n", structuresfilename.c_str());
+    // Check if there error is too many open files in the process and output a
+    // special error message
+    if (saved_errno == EMFILE) {
+      fprintf(stderr,
+        "Error: Increase the open file limit of the OS. "
+        "Directions are in the installation instructions\n");
+    }
     exit(EXIT_FAILURE);
   }
 
@@ -62,7 +71,7 @@ bool PCFG::loadGrammar(
     fprintf(stderr,
       "Error parsing structure file: %s! First line was not as expected!",
       structuresfilename.c_str());
-    exit(EXIT_FAILURE);  
+    exit(EXIT_FAILURE);
   }
 
   // Allocate structures and Nonterminal collection
@@ -71,7 +80,7 @@ bool PCFG::loadGrammar(
       "Error parsing structure file: %s! Not enough structure lines found!"
       "blanklinepos: %d  headerlines: %d\n",
       structuresfilename.c_str(), blanklinepos, headerlines);
-    exit(EXIT_FAILURE);    
+    exit(EXIT_FAILURE);
   }
   structures_size_ = static_cast<unsigned>(blanklinepos - headerlines - 1);
   structures_ = new Structure[structures_size_];
@@ -82,7 +91,7 @@ bool PCFG::loadGrammar(
   for (unsigned int i = 0; i < structures_size_; ++i) {
     std::string read_structure, read_source_ids;
     double read_probability;
-    if (grammartools::ReadStructureLine(structurefile, read_structure, 
+    if (grammartools::ReadStructureLine(structurefile, read_structure,
                           read_probability, read_source_ids)) {
       // Don't load giant structures -- they have extremely low probability
       // so you waste a lot of RAM (because they are large) for little benefit
@@ -91,22 +100,22 @@ bool PCFG::loadGrammar(
       // LoadStructure will start a chain reaction that will initialize
       // nonterminals and terminals associated with this structure
       if (!structures_[structure_counter].loadStructure(
-            read_structure, 
-            read_probability, 
+            read_structure,
+            read_probability,
             read_source_ids,
             nonterminal_collection_)) {
         fprintf(stderr,
           "Error calling LoadStructure on structure \"%s\" in file \"%s\"!\n",
-          read_structure.c_str(), 
+          read_structure.c_str(),
           structuresfilename.c_str());
-        exit(EXIT_FAILURE);          
+        exit(EXIT_FAILURE);
       }
       ++structure_counter;
     } else {
       fprintf(stderr,
         "Error parsing structure file: %s! Structure line was not as expected (check for previous errors)!",
         structuresfilename.c_str());
-      exit(EXIT_FAILURE);  
+      exit(EXIT_FAILURE);
     }
   }
   structures_size_ = structure_counter;
@@ -147,7 +156,7 @@ bool PCFG::generatePatterns(const double cutoff) const {
 // Print all strings above the given probability cutoff to stdout
 //
 // If accurate_probabilities is true, then Structure::generateStrings will
-// look up every string that is generated and output an accurate string 
+// look up every string that is generated and output an accurate string
 // probability.  We need to send that method an appropriate object that it
 // can make calls on.
 //
@@ -273,7 +282,7 @@ LookupData* PCFG::lookupSum(const std::string& inputstring) const {
     } else {
       // Clear up this structure's lookup data and don't update current data
       mpz_clear(structure_lookup->index);
-      delete structure_lookup;      
+      delete structure_lookup;
     }
   }
 
@@ -281,4 +290,3 @@ LookupData* PCFG::lookupSum(const std::string& inputstring) const {
   lookup_data->probability = total_probability;
   return lookup_data;
 }
-
