4b8c290c611fbf2a5786831c0f48cf4a3016a8cc
Billy Melicher <wrmelicher@gmail.com>
2016-01-18 09:50:05 -0500

    Forgot file

---
+++ binaries/randomness.h
@@ -0,0 +1,13 @@
+#ifndef RANDOMNESS_H
+#define RANDOMNESS_H
+
+#include <random>
+
+// Mersenne twister engine is used for random numbers here because they should
+// create the same numbers on different systems, and because they are fast. Be
+// sure to pass the engine by reference or pointer because there is mutable
+// state. The _64 stands for 64 bits of randomness. This is necessary because
+// with only 32 bits we might start repeating operations.
+typedef std::mt19937_64 RNG;
+
+#endif
